2ba7526d544778dedc238221dbaf6747bca44d9f
Cleferson Dannylo <clefersonnascimento8@gmail.com>
2021-09-27 15:24:54 -0300

    lasagna terminado

--- lasagna/lasagna.js
+++ lasagna/lasagna.js
@@ -1,6 +1,5 @@
-
-const PREPARATION_MINUTES_PER_LAYER = 2;
-
+const PREPARATION_MINUTES_PER_LAYER = 2
+export const EXPECTED_MINUTES_IN_OVEN = 40
 /**
  * Determines the amount of minutes the lasagna still needs to remain in the
  * oven to be properly prepared.
@@ -9,7 +8,7 @@ const PREPARATION_MINUTES_PER_LAYER = 2;
  * @returns {number} the number of minutes remaining
  */
 export function remainingMinutesInOven(actualMinutesInOven) {
-  throw new Error('Please implement the remainingMinutesInOven function');
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven
 }
 
 /**
@@ -19,7 +18,7 @@ export function remainingMinutesInOven(actualMinutesInOven) {
  * @returns {number} the total preparation time
  */
 export function preparationTimeInMinutes(numberOfLayers) {
-  throw new Error('Please implement the preparationTimeInMinutes function');
+  return PREPARATION_MINUTES_PER_LAYER * numberOfLayers
 }
 
 /**
@@ -31,5 +30,7 @@ export function preparationTimeInMinutes(numberOfLayers) {
  * @returns {number} the total working time
  */
 export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
-  throw new Error('Please implement the totalTimeInMinutes function');
+  return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven
 }
+
+// console.log(totalTimeInMinutes(2,30 ))
